d565bbdba6703f3d6c5441da6c12737c0c2829ee
aeduz24 <aeduz2024@gmail.com>
2024-06-18 20:17:32 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+import streamlit as st
+import pandas as pd
+
+# Sample DataFrame (replace with your DataFrame)
+
+# df1 = pd.DataFrame('2023-round1.csv')
+
+# Function to filter DataFrame based on user inputs
+def filter_data(df, gender, seat_type, quota, rank):
+    filtered_df = df[
+        (df['Gender'] == gender) &
+        (df['Seat Type'] == seat_type) &
+        (df['Quota'] == quota) &
+        
+        (df['Closing Rank']>= rank)
+    ]
+    return filtered_df
+
+st.title("AeduZ's Counselling Helper")
+
+# User inputs
+# year = st.number_input('Enter Year:', min_value=2000, step=1)
+# year=st.selectbox('select Year',[2023])
+# round_num = st.selectbox('Select Round:', [1,2,3,4,6])
+# gender = st.selectbox('Select Gender:', ['Gender-Neutral', 'Female-only (including Supernumerary)'])
+# seat_type = st.selectbox('Select Seat Type:', ['OPEN', 'EWS', 'OBC-NCL', 'SC', 'ST', 'OPEN (PwD)', 'OBC-NCL (PwD)', 'EWS (PwD)', 'ST (PwD)', 'SC (PwD)'])
+# quota = st.selectbox('Select Quota:', ['AI', 'HS', 'OS', 'GO', 'JK', 'LA'])
+# rank = st.number_input('Enter Rank:', min_value=0, step=1)
+st.sidebar.title('Filters')
+
+# User inputs in the sidebar
+seat_type = st.sidebar.selectbox('Select Category :', ['OPEN', 'EWS', 'OBC-NCL', 'SC', 'ST', 'OPEN (PwD)', 'OBC-NCL (PwD)', 'EWS (PwD)', 'ST (PwD)', 'SC (PwD)'])
+gender = st.sidebar.selectbox('Select Gender:', ['Gender-Neutral', 'Female-only (including Supernumerary)'])
+rank = st.sidebar.number_input('Enter Category Rank:', min_value=0, step=1)
+year = st.sidebar.selectbox('Select Year', [2023,2022,2021,2020])
+college=st.sidebar.selectbox('Select college',["IIT","NIT"])
+# round_num = st.sidebar.selectbox('Select Round:', [1, 2, 3, 4, 6])
+round_num = 6
+if college=="IIT":
+    quota = st.sidebar.selectbox('Select Quota:', ['All India (AI)'])
+if college=="NIT":
+    quota = st.sidebar.selectbox('Select Quota:', [' Other State (OS)','Home State (HS)', ' Goa (GO)', ' Jammu & Kashmir (JK)', ' Ladakh (LA)'])
+import re
+quota=re.findall(r'\((.*?)\)', quota)[0]
+# Load CSV based on user inputs
+filename = f'{year}_round{round_num}_{college.lower()}.csv'
+try:
+    df = pd.read_csv(filename)
+except FileNotFoundError:
+    st.error(f"Error: File '{filename}' not found.")
+    st.stop()
+df['Closing Rank']=df['Closing Rank'].astype(str)
+df['Opening Rank']=df['Opening Rank'].astype(str)
+df=df[~df['Closing Rank'].str.contains('P')]
+df=df[~df['Opening Rank'].str.contains('P')]
+df['Opening Rank']=df['Opening Rank'].astype(float)
+df['Closing Rank']=df['Closing Rank'].astype(float)
+nirf_iit=pd.read_csv('nirf_rank_iit.csv')
+nirf_nit=pd.read_csv('nirf_rank_nit.csv')
+
+if st.button('Find'):
+    # Filter DataFrame based on user inputs
+    filtered_df = filter_data(df, gender, seat_type, quota, rank)
+    if college=="IIT":
+        df_merge=filtered_df.merge(nirf_iit[['college', 'rank']], left_on='Institute', right_on='college', how='left')
+    if college=="NIT":
+        df_merge=filtered_df.merge(nirf_nit[['college', 'rank']], left_on='Institute', right_on='college', how='left')
+    # Display filtered DataFrame
+
+    df_final=df_merge.loc[:,['Institute', 'Academic Program Name','rank']].reset_index(drop=True)
+    df_final.fillna(1000,inplace=True)
+    st.write('Filtered DataFrame:')
+    st.write(df_final.sort_values(by='rank', ascending=True).reset_index(drop=True))
